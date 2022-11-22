46695bcf12c6a35ac3007e4a034f9f24c372be2b
Nastiakor <a.korotkova@yahoo.fr>
2022-11-22 16:28:54 +0100

    22/11

--- fetch.js
+++ fetch.js
@@ -4,22 +4,19 @@ fetch ('https://opendata.paris.fr/api/records/1.0/search/?dataset=que-faire-a-pa
 })
 .then(function(data) {
     console.log(data.facet_groups[0].facets[1].name);
-    let annee = data.facet_groups[0].facets[1].name;
-    document.getElementById("Annee").innerHTML = annee;
-<<<<<<< HEAD
+    //let annee = data.facet_groups[0].facets[1].name;
+    //document.getElementById("Annee").innerHTML = annee;
     console.log(data.facet_groups[13].facets[0].name); 
     let test = data.facet_groups[13].facets[0].name;
-    document.getElementById("Journée internationale").innerHTML = test;
+    let URL = "https://www.paris.fr/evenements/journee-internationale-de-lutte-contre-les-violences-faites-aux-femmes-27631"
+    document.getElementById("Journée internationale").innerHTML = test.link(URL);
+    console.log(data); 
+    console.log(records[0].fields.address_name)
 
 
-=======
-    
->>>>>>> 5d9f6f1957adac5e594b73bcf6015a24d6f69486
     // const h1 = document.getElementById ("Annee"); 
-    //
-    
 
-    //return donnees.map(function(a)) 
+    // return donnees.map(data.facet_groups[13]); 
 })
 .catch(function(error) {
 
@@ -33,3 +30,7 @@ function append(parent, el) {
 	return parent.appendChild(el);
 }
 
+
+let img = document.createElement("img");
+img.src = "Pic.jpg";
+document.getElementById("pic_spectacle").appendChild(img);
